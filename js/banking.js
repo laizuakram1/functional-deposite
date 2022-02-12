cf18cfe66be90bbf9e1a57a6b74527066fe30f50
laizuakram1 <laizuakram98@gmail.com>
2022-02-12 20:31:42 +0600

    deposit and withraw are functional

--- js/banking.js
+++ js/banking.js
@@ -7,6 +7,16 @@ function catchInput(num){
     return depositAmount;
 };
 
+// get input total 
+function inputTotal(inputId, depositAmount){
+    const depositTotal = document.getElementById(inputId);
+    
+    const depositPrevious = parseFloat(depositTotal.innerText);
+    const depositTotalBalance = depositAmount + depositPrevious;
+    depositTotal.innerText = depositTotalBalance;
+    return depositTotalBalance;
+}
+
 
 
 
@@ -17,12 +27,12 @@ document.getElementById('deposite-button').addEventListener('click', function(){
     const depositAmount = catchInput('deposite-input');
     
     // add deposite in deposit total
-    const depositTotal = document.getElementById('doposite-total');
+    // const depositTotal = document.getElementById('doposite-total');
     
-    const depositPrevious = parseFloat(depositTotal.innerText);
-    const depositTotalBalance = depositAmount + depositPrevious;
-    depositTotal.innerText = depositTotalBalance;
-
+    // const depositPrevious = parseFloat(depositTotal.innerText);
+    // const depositTotalBalance = depositAmount + depositPrevious;
+    // depositTotal.innerText = depositTotalBalance;
+     inputTotal('doposite-total', depositAmount);
     // udate balance
     const totalBalance = document.getElementById('balance-total');
     const totalBalancePrevious = parseFloat(totalBalance.innerText);
@@ -41,10 +51,12 @@ document.getElementById('withraw-button').addEventListener('click', function(){
     const withrawInputText = catchInput('withraw-input');
 
     // set withraw 
-const withrawTotal = document.getElementById('withraw-total');
-const withrawTotalPrevious = parseFloat(withrawTotal.innerText);
-const newWithrawBalance = withrawInputText + withrawTotalPrevious;
-withrawTotal.innerText = newWithrawBalance;
+// const withrawTotal = document.getElementById('withraw-total');
+// const withrawTotalPrevious = parseFloat(withrawTotal.innerText);
+// const newWithrawBalance = withrawInputText + withrawTotalPrevious;
+// withrawTotal.innerText = newWithrawBalance;
+
+inputTotal('withraw-total',withrawInputText );
 
 // update balance after withraw 
     const totalBalance = document.getElementById('balance-total');
